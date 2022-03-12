3808d15ef886e8f9d984cbd2c96f72cd3832f9f6
Shubham <shubh.behal76@gmail.com>
2022-03-12 08:23:32 -0500

    search page ui done

--- app/src/main/java/com/example/rapidchidori_mad5254_project/ui/fragments/SearchFragment.kt
+++ app/src/main/java/com/example/rapidchidori_mad5254_project/ui/fragments/SearchFragment.kt
@@ -6,14 +6,17 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.rapidchidori_mad5254_project.R
+import com.example.rapidchidori_mad5254_project.databinding.FragmentSearchBinding
+import com.example.rapidchidori_mad5254_project.databinding.FragmentUserProfileBinding
 
 class SearchFragment : Fragment() {
+    private lateinit var binding: FragmentSearchBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false)
+    ): View {
+        binding = FragmentSearchBinding.inflate(inflater, container, false)
+        return binding.root
     }
 }
