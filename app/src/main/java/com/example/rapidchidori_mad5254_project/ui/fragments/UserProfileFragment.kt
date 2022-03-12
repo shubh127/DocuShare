effe26b23cce0c8e3ab7b8d66201ec124a709dc3
Shubham <shubh.behal76@gmail.com>
2022-03-12 07:59:12 -0500

    profile page ui done

--- app/src/main/java/com/example/rapidchidori_mad5254_project/ui/fragments/UserProfileFragment.kt
+++ app/src/main/java/com/example/rapidchidori_mad5254_project/ui/fragments/UserProfileFragment.kt
@@ -1,18 +1,20 @@
 package com.example.rapidchidori_mad5254_project.ui.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.rapidchidori_mad5254_project.R
+import androidx.fragment.app.Fragment
+import com.example.rapidchidori_mad5254_project.databinding.FragmentUserProfileBinding
 
 class UserProfileFragment : Fragment() {
+    private lateinit var binding: FragmentUserProfileBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_user_profile, container, false)
+    ): View {
+        binding = FragmentUserProfileBinding.inflate(inflater, container, false)
+        return binding.root
     }
 }
